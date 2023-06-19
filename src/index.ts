1f8d24089b62c37158e94ee67c163eb4c8640fc7
pedro josé lima <pedrlimadev@gmail.com>
2023-06-19 10:37:16 -0300

    feat: add 'hello world' route

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
-const server = `'Nodemon' + 'TS-Node' server is running!`
+import * as express from "express";
+import { Request, Response } from "express";
 
-console.log('MESSAGE:', server);
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("hello world");
+});
+
+app.listen(3000, () => console.log("Server running on port 3000!"));
